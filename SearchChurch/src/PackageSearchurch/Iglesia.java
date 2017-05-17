7a4defeec5754321ec5d2c8018c69b6a2c5549de
Esteban201620 <estebiosorio@gmail.com>
2017-05-16 19:14:42 -0500

    Add files via upload

---
+++ SearchChurch/src/PackageSearchurch/Iglesia.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PackageSearchurch;
+
+/**
+ *
+ * @author esteban
+ */
+public class Iglesia {
+    float latitud;
+    float longitud;
+    String nombre;
+}
